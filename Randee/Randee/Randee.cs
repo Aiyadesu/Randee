3c5a3ab966ee011261a530853f2694bb640c3dfa
Richard Lai <richard08192@hotmail.com>
2017-03-30 00:07:38 +1000

    Initial commit of a Windows Form App from Visual Studio 2017

---
+++ Randee/Randee/Randee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randee
+{
+    public partial class Randee : Form
+    {
+        public Randee()
+        {
+            InitializeComponent();
+        }
+
+        private void Randee_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
