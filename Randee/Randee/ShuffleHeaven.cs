47e2dd75b3d711e6013e90e9f6aa7846cf255827
Richard Lai <richard08192@hotmail.com>
2017-04-02 00:49:44 +1000

    Implemented the RNGCryptoServiceProvider as per the example in the official documentation.

--- Randee/Randee/ShuffleHeaven.cs
+++ Randee/Randee/ShuffleHeaven.cs
@@ -9,26 +9,66 @@ namespace Randee
 {
     class ShuffleHeaven
     {
-        // Class Fields
+        /* Class Fields */
+        private static RNGCryptoServiceProvider crng = new RNGCryptoServiceProvider();
+
         private byte[] randomNumbers;
 
-        // Default constructor
+
+
+        /* Default Constructor */
         public ShuffleHeaven()
         {
-            int maxNumbers = 14;
-            // Generates 10 random numbers
-            using (RNGCryptoServiceProvider crng = new RNGCryptoServiceProvider())
+            randomNumbers = new byte[14];
+            for(int i = 0; i < 14; i++)
             {
-                randomNumbers = new byte[maxNumbers];
-
-                for(int i = 1; i <= maxNumbers; i++)
-                {
-                    crng.GetBytes(randomNumbers);
-
-                    int value = BitConverter.ToInt32(randomNumbers, 0);
-                    Console.WriteLine(i.ToString() + ")" + value);
-                }
+                randomNumbers[i] = GenerateNumber(100);
             }
+
+            foreach(byte number in randomNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        /* Main Functions */
+
+
+
+        // Use to generate a random number between the range of 1 and the 'maxRange'.
+        // The input parameter is the 'maxRange' i.e To simulate rolling a 6-sided you would input "6" as the 'maxRange' parameter.
+        public static byte GenerateNumber(byte maxRange)
+        {
+            if(maxRange <= 0)
+            {
+                throw new ArgumentOutOfRangeException("maxRange is invalid! Cannot be less than or equal to 0");
+            }
+
+            // Create a byte array to hold the random value.
+            byte[] randomNumber = new byte[1]; 
+
+            do
+            {
+                // Fill the array with a random value.
+                crng.GetBytes(randomNumber);
+            }
+
+            while (!IsUniformlyDistributed(randomNumber[0], maxRange));
+
+            return (byte)((randomNumber[0] % maxRange) + 1); // Adjusted for zero-indexed memery
+        }
+
+        /* Helper Functions */
+
+
+
+        // Use to ensure each random number returned has an equal probability of occurring i.e "Uniformly Distributed"
+        // Ripped from https://msdn.microsoft.com/en-us/library/system.security.cryptography.rngcryptoserviceprovider(v=vs.110).aspx
+        private static bool IsUniformlyDistributed(byte roll, byte numSides)
+        {
+            int fullSetsOfValues = Byte.MaxValue / numSides;
+
+            return roll < numSides * fullSetsOfValues;
         }
 
         
