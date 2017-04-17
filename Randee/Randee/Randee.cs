3d7a50291aa8f505b28f0c553c01de9c809cb1d0
Richard Lai <richard08192@hotmail.com>
2017-04-18 08:27:37 +1000

    Added a function to create settings for the application that can be read from later.

--- Randee/Randee/Randee.cs
+++ Randee/Randee/Randee.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace Randee
 {
@@ -19,6 +20,8 @@ namespace Randee
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
 
+        private string appDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+
         /* Externally implemented functions (i.e from 'user32.dll') */
         [DllImport("user32.dll")] 
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
@@ -26,21 +29,30 @@ namespace Randee
         [DllImport("user32.dll")]
         public static extern bool ReleaseCapture();
 
+
+
         // Default Constructor
         public Randee()
         {
             InitializeComponent();
+            CreateSettingsFile();
         }
+
+
 
         private void Randee_Load(object sender, EventArgs e)
         {
 
         }
 
+
+
         private void titleBar_Click(object sender, EventArgs e)
         {
 
         }
+
+
 
         // The main event function to be called
         private void generateNumber_Click(object sender, EventArgs e)
@@ -57,20 +69,28 @@ namespace Randee
             }
         }
 
+
+
         private void rangeInput_ValueChanged(object sender, EventArgs e)
         {
 
         }
+
+
 
         private void numberDisplay_Click(object sender, EventArgs e)
         {
 
         }
 
+
+
         private void minRangeInput_ValueChanged(object sender, EventArgs e)
         {
 
         }
+
+
 
         private void Randee_MouseDown(object sender, MouseEventArgs e)
         {
@@ -78,6 +98,57 @@ namespace Randee
             {
                 ReleaseCapture();
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+
+
+        /* 
+         * Creates a file to store settings if there is no settings currently set, otherwise read from the current settings
+         * Source: https://msdn.microsoft.com/en-us/library/d62kzs03(v=vs.110).aspx
+         */
+        private void CreateSettingsFile()
+        {
+            // The path to create or find the settings file
+            string settingsPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\settings.txt";
+
+            // Used to store the settings
+            string settings = "";
+
+            /* Current settings already exist */
+            // Check if settings are already set
+            if(File.Exists(settingsPath))
+            {
+
+                // Open the 'settings.txt' file and then store the contents of the file in a string
+                using (StreamReader streamReader = File.OpenText(settingsPath))
+                {
+
+                    string currentLine = "";
+                    settings = "";
+
+                    while ((currentLine = streamReader.ReadLine()) != null)
+                    {
+                        settings += currentLine;
+                    }
+
+                }
+
+                return;
+
+            }
+
+
+
+            /* No current settings detected */
+            // Adds whether the "RANDOM_ORG_API" environment variable is found or not to the settings
+            settings = "RANDOM_ORG_API = " + (Environment.GetEnvironmentVariable("RANDOM_ORG_API") == null ? "0" : "1") + "\r\n";
+
+            // Create a 'settings.txt' file that stores the users settings
+            using (FileStream fileStream = File.Create(settingsPath))
+            {
+                Byte[] settingsData = new UTF8Encoding(true).GetBytes(settings);
+                fileStream.Write(settingsData, 0, settingsData.Length);
             }
         }
     }
