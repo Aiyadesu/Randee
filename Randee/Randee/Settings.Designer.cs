64544e86c6522a66bc8b6d24690274d200fd6274
Richard Lai <richard08192@hotmail.com>
2017-04-21 05:00:39 +1000

    Improved the 'Save API Key' functionality.

--- Randee/Randee/Settings.Designer.cs
+++ Randee/Randee/Settings.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.labelAPIKey = new System.Windows.Forms.Label();
             this.textboxAPIKey = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSaveKey = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelAPIKey
@@ -55,23 +55,23 @@
             this.textboxAPIKey.WordWrap = false;
             this.textboxAPIKey.Enter += new System.EventHandler(this.textboxAPIKey_Enter);
             this.textboxAPIKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textboxAPIKey_KeyDown);
-            this.textboxAPIKey.Leave += new System.EventHandler(this.textboxAPIKey_Leave);
             // 
-            // button1
+            // buttonSaveKey
             // 
-            this.button1.Location = new System.Drawing.Point(59, 203);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonSaveKey.Location = new System.Drawing.Point(165, 118);
+            this.buttonSaveKey.Name = "buttonSaveKey";
+            this.buttonSaveKey.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveKey.TabIndex = 2;
+            this.buttonSaveKey.Text = "Save Key";
+            this.buttonSaveKey.UseVisualStyleBackColor = true;
+            this.buttonSaveKey.Click += new System.EventHandler(this.buttonSaveKey_Click);
             // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(420, 420);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSaveKey);
             this.Controls.Add(this.textboxAPIKey);
             this.Controls.Add(this.labelAPIKey);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -88,6 +88,6 @@
 
         private System.Windows.Forms.Label labelAPIKey;
         private System.Windows.Forms.TextBox textboxAPIKey;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSaveKey;
     }
 }
--- Randee/Randee/Settings.cs
+++ Randee/Randee/Settings.cs
@@ -16,6 +16,7 @@ namespace Randee
         {
             InitializeComponent();
 
+            // Set the 'API Key' label text depending on whether the environment variable for the API Key already exists.
             if(Environment.GetEnvironmentVariable(Randee.API_KEY) == null)
             {
                 labelAPIKey.Text = "Enter a Random.org API key!";
@@ -30,8 +31,51 @@ namespace Randee
 
 
 
-        /* Validates the user input when focus is lost */
-        private void textboxAPIKey_Leave(object sender, EventArgs e)
+
+        /* Event Handlers */
+        
+
+
+        // Resets the text box colour
+        private void textboxAPIKey_Enter(object sender, EventArgs e)
+        {
+            textboxAPIKey.ForeColor = Color.Black;
+            textboxAPIKey.Text = "";
+        }
+
+
+
+        // Attempts to save the API Key when the 'Enter' key is pressed from within the textbox 
+        private void textboxAPIKey_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                SaveKey();
+            }
+        }
+
+
+
+        // Attempts to save the API Key when the user presses the 'Save Key' button
+        private void buttonSaveKey_Click(object sender, EventArgs e)
+        {
+            SaveKey();
+        }
+
+
+
+        /* Support functions */
+
+
+
+        /* Attempts to save the API Key.
+         * Sets an environment variable which contains the Random.org API key.
+         * Prompts the user if the key given is not a valid key.
+         */
+        private void SaveKey()
         {
             if(IsInputValid(textboxAPIKey.Text))
             {
@@ -40,51 +84,33 @@ namespace Randee
             }
 
             textboxAPIKey.ForeColor = Color.Red;
-            textboxAPIKey.Text = "Invalid key detected! Please check your key and then again!";
-
-
-           
+            textboxAPIKey.Text = "Invalid key detected! Please check your key and try again!";
         }
 
 
 
+        /* Validates the API Key.
+         * Returns true if the key contains only letters, digits and the hyphen (-) symbol.
+         * Otherwise, returns false.
+         */
         private bool IsInputValid(string givenInput)
         {
             string userInput = givenInput;
 
-            foreach(char character in userInput)
+            foreach (char character in givenInput)
             {
 
-                if(!Char.IsLetterOrDigit(character))
+                if (!Char.IsLetterOrDigit(character))
                 {
-                    if(character != '-')
+                    if (character != '-')
                     {
                         return false;
                     }
                 }
-               
+
             }
 
             return true;
-        }
-
-
-
-        private void textboxAPIKey_Enter(object sender, EventArgs e)
-        {
-            textboxAPIKey.ForeColor = Color.Black;
-            textboxAPIKey.Text = "";
-        }
-
-        private void textboxAPIKey_KeyDown(object sender, KeyEventArgs e)
-        {
-            if(e.KeyCode == Keys.Enter)
-            {
-                e.Handled = true;
-                e.SuppressKeyPress = true;
-
-                labelAPIKey.Focus();
-            }
         }
     }
 }
