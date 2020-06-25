bc1390816ae4eb598d7fefec18ad53f281a1ecca
Richard Lai <richard08192@hotmail.com>
2020-06-26 00:11:50 +1000

    Renamed folder 'Randee' to 'Windows'

---
+++ Windows/Randee/Randee.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Randee
+{
+    public partial class Randee : Form
+    {
+        /* Windows Constants */
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        /* Randee Constants */
+        public const string API_KEY = "RANDOM_ORG_API";
+        private const string TITLE_HOME = "Randee - Home";
+        private const string TITLE_SETTINGS = "Randee - Settings";
+
+        private const string ERROR_NUMBER = "Number provided is not true random.";
+        private const string ERROR_QUOTA = "Cannot retrieve the Daily Quota.";
+
+        /* Other Pages/Forms */
+        private Settings settingsForm = new Settings();
+
+        /* Randee Members */
+        private string historyPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\history.txt";
+        private string dateTimeAppOpened = "Numbers generated history from: " + DateTime.Now.ToString() + "\r\n";
+        private string generatedNumbers;
+
+        /* Colours */
+        public static Color TITLE_BAR_BACK_COLOUR = Color.FromArgb(64, 64, 64);
+        public static Color WINDOW_BACK_COLOUR    = Color.FromArgb(128, 128, 128);
+        public static Color TEXT_COLOUR           = Color.FromArgb(230, 230, 230);
+        public static Color INPUT_COLOUR          = Color.FromArgb(255, 255, 255);
+        public static Color RESULTS_COLOUR        = Color.FromArgb(0, 153, 0);
+        public static Color ERROR_COLOUR          = Color.FromArgb(204, 0, 0);
+
+
+
+        /* Externally implemented functions (i.e from 'user32.dll') */
+        [DllImport("user32.dll")] 
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+
+        public Randee()
+        {
+            InitializeComponent();
+
+            /* Set the "dark" theme of the application */
+            BackColor = TITLE_BAR_BACK_COLOUR;
+
+            // Set the display colours
+            labelNumberDisplay.ForeColor   = TEXT_COLOUR;
+            labelTitle.ForeColor           = TEXT_COLOUR;
+            labelCustomRange.ForeColor     = TEXT_COLOUR;
+            labelFrom.ForeColor            = TEXT_COLOUR;
+            labelTo.ForeColor              = TEXT_COLOUR;
+            labelNumberOfNumbers.ForeColor = TEXT_COLOUR;
+            labelQuotaTitle.ForeColor      = TEXT_COLOUR;
+
+            // Set the input colours
+            inputMinRange.BackColor        = WINDOW_BACK_COLOUR;
+            inputMinRange.ForeColor        = INPUT_COLOUR;
+
+            inputMaxRange.BackColor        = WINDOW_BACK_COLOUR;
+            inputMaxRange.ForeColor        = INPUT_COLOUR;
+
+            inputNumberOfNumbers.BackColor = WINDOW_BACK_COLOUR;
+            inputNumberOfNumbers.ForeColor = INPUT_COLOUR;
+
+            buttonGenerateNumber.BackColor = WINDOW_BACK_COLOUR;
+            buttonGenerateNumber.ForeColor = INPUT_COLOUR;
+
+            buttonCheckQuota.BackColor     = WINDOW_BACK_COLOUR;
+            buttonCheckQuota.ForeColor     = INPUT_COLOUR;
+
+            // Set the result colours
+            labelNumberDisplay.ForeColor   = RESULTS_COLOUR;
+            labelMultipleNumbers.ForeColor = RESULTS_COLOUR;
+
+            labelBitsLeft.ForeColor        = RESULTS_COLOUR;
+            labelRequestsLeft.ForeColor    = RESULTS_COLOUR;
+
+            // Set the error message colour
+            labelErrorMessage.ForeColor    = ERROR_COLOUR;
+
+            // Clear the default text
+            HideOutputScreen();
+
+            // Set the 'Title' text
+            UpdateTitle(TITLE_HOME);
+
+            // Set the 'Check Quota' button default properties if necessary
+            if(settingsForm.IsAPIKeySet())
+            {
+                buttonCheckQuota.Visible = true;
+            } else
+            {
+                buttonGenerateNumber.Location = new Point(165, 377);
+            }
+        }
+
+
+
+        /* Event Handlers */
+
+
+
+        /*
+         * Generates a true or pseudo random number 
+         * depending on whether the Random.org API Key has been set.
+         */
+        private void buttonGenerateNumber_Click(object sender, EventArgs e)
+        {
+            ChangeCursor();
+            HideOutputScreen();
+            buttonGenerateNumber.Enabled = false; // Lock the button
+
+            List<int> generatedNumbers;
+            string formattedNumbers = String.Empty;
+
+
+
+            /* Generates a true or pseudo random number, depending on whether the Random.org API Key is set or not */
+            if (settingsForm.IsAPIKeySet())
+            {
+                generatedNumbers = ShuffleHeaven.GetTrueRandomNumber((int)inputNumberOfNumbers.Value, (int)inputMinRange.Value, (int)inputMaxRange.Value);
+
+                // Shows an error message if 'ShuffleHeaven' throws an exception
+                HasShuffleHeavenThrownException(ERROR_NUMBER);
+            }
+            else
+            {
+                /* Generates a pseudo random number */
+                generatedNumbers = ShuffleHeaven.GeneratePseudoRandomNumber((int)inputNumberOfNumbers.Value, (int)inputMinRange.Value, (int)inputMaxRange.Value);
+            }
+
+
+
+            formattedNumbers = ConvertListIntsToString(generatedNumbers);
+
+            labelNumberDisplay.Text =
+                inputNumberOfNumbers.Value > 1 ?
+                "Your random numbers are: " : "Your random number is: " + formattedNumbers;
+
+            labelMultipleNumbers.Text =
+                inputNumberOfNumbers.Value > 1 ?
+                formattedNumbers : String.Empty;
+
+            ShowNumber();
+
+            AddToLog(formattedNumbers);
+
+            buttonGenerateNumber.Enabled = true; // Unlock the button   
+        }
+
+
+
+        private void buttonCheckQuota_Click(object sender, EventArgs e)
+        {
+            ChangeCursor();
+            HideOutputScreen();
+
+            ShuffleHeaven.GetUsage();
+
+            if (!HasShuffleHeavenThrownException(ERROR_QUOTA))
+            {
+                labelBitsLeft.Text = "Bits Left: " + ShuffleHeaven.GetBitsLeft();
+                labelRequestsLeft.Text = "Requests Left: " + ShuffleHeaven.GetRequestsLeft();
+
+                ShowQuota();
+            }
+        }
+
+
+
+        /* 
+         * Moves the 'Randee' window around
+         * if the user is holding down the left
+         * mouse button in the title bar area 
+         */
+        private void Randee_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+
+
+        /*
+         * Moves the 'other' forms along with the 'Randee' window
+         */
+        private void Randee_LocationChanged(object sender, EventArgs e)
+        {
+            Point window = Location;
+            window.Offset(0, 42);
+
+            settingsForm.Location = window;
+        }
+
+
+
+        /*
+         * Changes the buttons colour. Default is colour is 'c0c0c0'.
+         * Changes to 'FFFFFF' if the button is hovered.
+         * Changes back to the default colour when no longer hovered.
+         */
+
+        /* 'Close' button functions */
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            if(settingsForm.GetKeepHistory())
+            {
+                OutputLogFile();
+            }
+
+            Application.Exit();
+        }
+
+        private void buttonClose_MouseOver(object sender, EventArgs e)
+        {
+            buttonClose.BackgroundImage = Properties.Resources.close_button_FFFFFF;
+        }
+
+        private void buttonClose_Leave(object sender, EventArgs e)
+        {
+            buttonClose.BackgroundImage = Properties.Resources.close_button_c0c0c0;
+        }
+
+
+
+        /* 'Home' button functions */
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            if(settingsForm.Visible)
+            {
+                settingsForm.Hide();
+            }
+
+            UpdateTitle(TITLE_HOME);
+        }
+
+        private void buttonHome_MouseOver(object sender, EventArgs e)
+        {
+            buttonHome.BackgroundImage = Properties.Resources.home_button_FFFFFFF;
+        }
+
+        private void buttonHome_Leave(object sender, EventArgs e)
+        {
+            buttonHome.BackgroundImage = Properties.Resources.home_button_c0c0c0;
+        }
+
+
+
+        /* 'Settings' button functions */
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            HideOutputScreen();
+            UpdateTitle(TITLE_SETTINGS);
+
+            settingsForm.BackColor = BackColor;
+
+            Point window = Location;
+            window.Offset(0, 42);
+
+            settingsForm.Location = window;
+
+            if (!settingsForm.Visible)
+            {
+                settingsForm.Show(this);
+            }
+        }
+
+        private void buttonSettings_MouseOver(object sender, EventArgs e)
+        {
+            buttonSettings.BackgroundImage = Properties.Resources.settings_button_FFFFFF;
+        }
+
+        private void buttonSettings_Leave(object sender, EventArgs e)
+        {
+            buttonSettings.BackgroundImage = Properties.Resources.settings_button_c0c0c0;
+        }
+
+
+
+        /* Main Functions */
+        private void AddToLog(string number)
+        {
+            generatedNumbers += number + settingsForm.GetSeperator() + "\r\n";
+        }
+
+
+
+        /// <summary>
+        /// Outputs the history of generated numbers by "session". 
+        /// i.e Everytime the application is closed a "session" is ended.
+        /// </summary>
+        private void OutputLogFile()
+        {
+            string history = dateTimeAppOpened + generatedNumbers;
+
+            if(!File.Exists(historyPath))
+            {
+                using (FileStream fileStream = File.Create(historyPath))
+                {
+                    Byte[] historyData = new UTF8Encoding(true).GetBytes(history + "\r\n\r\n");
+                    fileStream.Write(historyData, 0, historyData.Length);
+                }
+                return;
+            }
+
+            using (StreamWriter streamWriter = File.AppendText(historyPath))
+            {
+                streamWriter.WriteLine(history + "\r\n\r\n");
+            }
+        }
+
+        /* Support Functions */
+
+        private void UpdateTitle(string newTitle)
+        {
+            labelTitle.Text = newTitle;
+        }
+
+
+
+        private void HideErrorMessage()
+        {
+            labelErrorMessage.Visible = false;
+        }
+
+
+
+        private void ShowErrorMessage()
+        {
+            labelErrorMessage.Visible = true;
+        }
+
+
+
+        private void HideQuota()
+        {
+            labelQuotaTitle.Visible = false;
+            labelBitsLeft.Visible = false;
+            labelRequestsLeft.Visible = false;
+        }
+
+
+
+        private void ShowQuota()
+        {
+            labelQuotaTitle.Visible = true;
+            labelBitsLeft.Visible = true;
+            labelRequestsLeft.Visible = true;
+        }
+
+
+
+        private void HideNumber()
+        {
+            labelNumberDisplay.Visible = false;
+            labelMultipleNumbers.Visible = false;
+        }
+
+
+
+        private void ShowNumber()
+        {
+            labelNumberDisplay.Visible = true;
+            labelMultipleNumbers.Visible = true;
+        }
+
+
+
+        private void HideOutputScreen()
+        {
+            HideQuota();
+            HideNumber();
+            HideErrorMessage();
+        }
+
+
+
+        private void ChangeCursor()
+        {
+            Cursor.Current = 
+                Cursor.Current == Cursors.Default ? 
+                Cursors.WaitCursor : Cursors.Default;
+        }
+
+
+        /// <summary>
+        /// Checks whether an exception was thrown by 'ShuffleHeaven' 
+        /// and sets the error message given by 'errorMesssage' parameter.
+        /// 
+        /// Call this function after a function is called from 'ShuffleHeaven' that
+        /// retrieves a response from Random.org.
+        /// </summary>
+        /// <param name="errorMessage">The string that lets the user know what is wrong.</param>
+        /// <returns></returns>
+        private bool HasShuffleHeavenThrownException(string errorMessage)
+        {
+            bool hasException = ShuffleHeaven.GetExceptionThrown();
+
+            if (hasException)
+            {
+                labelErrorMessage.Text = 
+                    "An issue with the connectivity was detected. \r\n" + errorMessage;
+                ShowErrorMessage();
+            }
+
+            return hasException;
+        }
+
+
+        /// <summary>
+        /// Formats the numbers within the list with the specified seperator in the 'Settings'
+        /// and returns the string represntation.
+        /// </summary>
+        /// <param name="list">The list to convert.</param>
+        /// <returns></returns>
+        private string ConvertListIntsToString(List<int> list)
+        {
+            string newString = String.Empty;
+
+            foreach (int integer in list)
+            {
+                newString += integer.ToString() + settingsForm.GetSeperator();
+            }
+
+            newString = newString.Remove(newString.Length - 1);
+
+            return newString;
+        }
+    }
+}
---
+++ Windows/Randee/ShuffleHeaven.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Randee
+{
+    class ShuffleHeaven
+    {
+        /* Class Members */
+
+        private static RNGCryptoServiceProvider crng    = new RNGCryptoServiceProvider();
+        private static System.Random prng               = new System.Random();
+        private static WebClient webClient              = new WebClient();
+
+        /* 'ShuffleHeaven' constants */
+        private static readonly string REQUEST_ID           = "1414";
+        private static readonly string ENVIRONMENT_VAR_NAME = "RANDOM_ORG_API";
+
+        /* Random.org Information */
+        private static int bitsLeft;
+        private static int requestsLeft;
+
+        private static string numbers;
+        private static DateTime sessionStartTime = DateTime.Now.ToUniversalTime();
+        private static DateTime advisedRequestTime;
+        private static DateTime defaultDateTime;
+
+        private static bool exceptionThrown;
+        
+
+
+        /* Default Constructor */
+
+        public ShuffleHeaven()
+        {
+            // do nothing for now
+        }
+
+
+
+        /* Main Functions */
+
+
+
+        /*
+         * Generates 'numberOfNumbers' pseudo random number(s) between the range of 'minRange' and 'maxRange'
+         */
+        public static List<int> GeneratePseudoRandomNumber(int numberOfNumbers, int minRange, int maxRange)
+        {
+            System.Random sprng = new System.Random();
+            List<int> result = new List<int>();
+
+            for(int numbersAdded = 0; numbersAdded < numberOfNumbers; numbersAdded++)
+            {
+                result.Add(sprng.Next(minRange, maxRange));
+            }
+
+            return result;
+        }
+
+
+
+        /// <summary>
+        /// Use the Random.org API to generate random numbers.
+        /// 
+        /// Guidelines: (https://api.random.org/guidelines)
+        /// 1) If possible, do not issue multiple simultaneous requests (Doesn't matter if client is single-threaded).
+        /// 2) Fetch numbers in blocks as large as possible, this is more efficient than issuing a separate request for every single number.
+        /// 3) If the client is not a real-time application, use a long timeout value for your requests, ideally a couple of minutes.
+        /// 4) If the daily request or bit limit is exceeded, the client must back off until midnight UTC. Random.org will respond with a
+        ///    402 or 403 error code, if your API key does not have enough requests or bits left. Your key's current usage is included in nearly all API
+        ///    responses, so the client can easily track how many of each it has left.
+        /// 5) Obey the advisory delays requested by Random.org in the API responses.
+        /// </summary>
+        /// <param name="numberOfNumbers"></param>
+        /// <param name="minRange"></param>
+        /// <param name="maxRange"></param>
+        public static List<int> GetTrueRandomNumber(int numberOfNumbers, int minRange, int maxRange)
+        {
+            numbers = String.Empty;
+            SetExceptionThrown(false);
+
+            if (GetRequestsLeft() == 0) return GeneratePseudoRandomNumber(numberOfNumbers, minRange, maxRange);
+            if (GetBitsLeft() <= 0) return GeneratePseudoRandomNumber(numberOfNumbers, minRange, maxRange);
+            if (GetAdvisedRequestTime() != defaultDateTime && DateTime.Now.ToUniversalTime() < GetAdvisedRequestTime()) return GeneratePseudoRandomNumber(numberOfNumbers, minRange, maxRange);
+
+            string response = String.Empty;
+
+            try
+            {
+                response = webClient.UploadString("https://api.random.org/json-rpc/1/invoke",
+                "{\"jsonrpc\":\"2.0\",\"method\":\"generateIntegers\",\"params\":{\"apiKey\":\""
+                + Environment.GetEnvironmentVariable(ENVIRONMENT_VAR_NAME, EnvironmentVariableTarget.User) + "\",\"n\":"
+                + numberOfNumbers.ToString() + ",\"min\":" + minRange.ToString() + ",\"max\":" + maxRange.ToString()
+                + ",\"replacement\":true,\"base\":10},\"id\":" + REQUEST_ID + "}");
+            }
+            catch (WebException)
+            {
+                SetExceptionThrown(true);
+
+                return GeneratePseudoRandomNumber(numberOfNumbers, minRange, maxRange);
+            }
+
+            TrueRandomObject tro = JsonConvert.DeserializeObject<TrueRandomObject>(response);
+
+            SetBitsLeft(tro.result.bitsLeft);
+            SetRequestsLeft(tro.result.requestsLeft);
+            SetAdvisedRequestTime(tro.result.advisoryDelay);
+
+            return tro.result.random.data;
+        }
+
+
+
+        /* Returns information related to the usage of the set API Key. */
+        public static void GetUsage()
+        {
+            SetExceptionThrown(false);
+
+            try
+            {
+                string usage = webClient.UploadString("https://api.random.org/json-rpc/1/invoke",
+                "{\"jsonrpc\":\"2.0\",\"method\":\"getUsage\",\"params\":{\"apiKey\":\""
+                + Environment.GetEnvironmentVariable(ENVIRONMENT_VAR_NAME, EnvironmentVariableTarget.User) + "\"},\"id\":" + REQUEST_ID + "}");
+
+                TrueRandomObject tro = JsonConvert.DeserializeObject<TrueRandomObject>(usage);
+
+                SetBitsLeft(tro.result.bitsLeft);
+                SetRequestsLeft(tro.result.requestsLeft);
+            }
+            catch (WebException)
+            {
+                SetExceptionThrown(true);
+            }
+        }
+
+
+
+        /* Getters & Setters */
+        public static int GetBitsLeft()
+        {
+            return bitsLeft;
+        }
+
+        private static void SetBitsLeft(int bitsRemaining)
+        {
+            bitsLeft = bitsRemaining;
+        }
+
+
+
+        public static int GetRequestsLeft()
+        {
+            return requestsLeft;
+        }
+
+        private static void SetRequestsLeft(int requestsRemaining)
+        {
+            requestsLeft = requestsRemaining;
+        }
+
+
+
+        private static DateTime GetAdvisedRequestTime()
+        {
+            return advisedRequestTime;
+        }
+
+        private static void SetAdvisedRequestTime(int advisoryDelay)
+        {
+            advisedRequestTime = DateTime.Now.ToUniversalTime().AddMilliseconds(advisoryDelay);
+        }
+
+
+
+        public static bool GetExceptionThrown()
+        {
+            return exceptionThrown;
+        }
+
+        private static void SetExceptionThrown(bool exceptionThrownValue)
+        {
+            exceptionThrown = exceptionThrownValue;
+        }
+    }
+}
